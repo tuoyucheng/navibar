74e524217b0b56a0a5beed4a4ad6af492dd78591
tuoyucheng <amazon60@icloud.com>
2023-05-15 23:13:56 +0900

    5/15

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ $(function () {
       $('.menu').addClass('fixed');
       $('.menu').addClass('menuchange');
       $('.menu').removeClass('menufirst');
-    } else if($(this).scrollTop() === 0) {
+    } else if ($(this).scrollTop() === 0) {
       $('.menu').removeClass('menuchange');
       $('.menu').addClass('menufirst');
     } else {
       $('.menu').removeClass('fixed');
-      // $('.menu').removeClass('menuchange');
-      // $('.menu').addClass('menufirst');
     }
   });
 });
